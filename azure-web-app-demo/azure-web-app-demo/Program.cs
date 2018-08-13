a4e5b38d640e554d802021f7127283f441ca8153
bpelikan <bartoszpelikan@gmail.com>
2018-08-13 13:31:17 +0200

    Add database migration

--- azure-web-app-demo/azure-web-app-demo/Program.cs
+++ azure-web-app-demo/azure-web-app-demo/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using azure_web_app_demo.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace azure_web_app_demo
@@ -14,7 +17,18 @@ namespace azure_web_app_demo
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            MigrateDatabase(host);
+            host.Run();
+        }
+
+        public static void MigrateDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
